a8946ac36953c4f6c06abd42d976b9549dc6b50c
emer mcmahon <emerod0000@gmail.com>
2018-06-22 09:38:26 +0100

    adding flayer function

--- js/app.js
+++ js/app.js
@@ -21,10 +21,37 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-princess-girl.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+player.prototype.handleInput = function(allowedKeys[e.keyCode]) {
+    
+
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
